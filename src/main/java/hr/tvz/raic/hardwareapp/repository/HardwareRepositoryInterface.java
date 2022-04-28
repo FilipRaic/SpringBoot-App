38becc243ccf6b288d6c533fb2b2a3b9bfbe4bc9
Filip <raic.filip@gmail.com>
2022-04-28 21:27:58 +0200

    Feat/Add in-memory H2 database

--- src/main/java/hr/tvz/raic/hardwareapp/repository/HardwareRepositoryInterface.java
+++ src/main/java/hr/tvz/raic/hardwareapp/repository/HardwareRepositoryInterface.java
@@ -1,11 +1,15 @@
 package hr.tvz.raic.hardwareapp.repository;
 
 import hr.tvz.raic.hardwareapp.model.GenericClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface GenericRepository<T extends GenericClass> {
+public interface GenericRepository<T extends GenericClass> extends CrudRepository<T, Long> {
     List<T> findAll();
     Optional<T> findByCode(String code);
     void create(T object);
